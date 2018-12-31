b7722b908efacadd37867b51fc0fe0c34e71dd10
Pete Dietl <petedietl@gmail.com>
2018-12-31 17:21:05 -0600

    consolidate instruction format

--- main.c
+++ main.c
@@ -4,17 +4,51 @@
 #include <getopt.h>
 #include "arch.h"
 
-void (*op_functions[NUM_OP_CODES])(FILE *) = {0};
+void out_handler(FILE *fp);
+
+enum asm_instr_arg_type {
+    EMPTY,
+    REG,
+    INT,
+    REG_OR_INT
+};
+
+#define MAX_NUM_ARGS_PLUS_ONE 4
+
+struct asm_instr {
+    const char *mnemonic;
+    enum asm_instr_arg_type arg_types[MAX_NUM_ARGS_PLUS_ONE];
+    void (*special_handler)(FILE *fp);
+};
+
+struct asm_instr ops[] = {
+    {"hlt",     {0}},
+    {"set",     {REG, REG_OR_INT}},
+    {"push",    {REG_OR_INT}},
+    {"pop",     {REG_OR_INT}},
+    {"eq",      {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"gt",      {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"jmp",     {REG_OR_INT}},
+    {"jt",      {REG_OR_INT, REG_OR_INT}},
+    {"jf",      {REG_OR_INT, REG_OR_INT}},
+    {"add",     {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"mult",    {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"mod",     {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"and",     {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"or",      {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"not",     {REG_OR_INT, REG_OR_INT}},
+    {"rmem",    {REG_OR_INT, REG_OR_INT}},
+    {"wmem",    {REG_OR_INT, REG_OR_INT}},
+    {"call",    {REG_OR_INT}},
+    {"ret",     {0}},
+    {"out",     {REG_OR_INT}, out_handler},
+    {"in",      {REG_OR_INT}},
+    {"noop",    {0}}
+};
+
 void disass(FILE *fp);
 
 void not_implemented(enum opcode);
-void halt(FILE *fp);
-void jmp(FILE *fp);
-void out(FILE *fp);
-void noop(FILE *fp);
-void jt(FILE *fp);
-void jf(FILE *fp);
-void set(FILE *fp);
 
 int line = 0;
 bool no_offset = false;
@@ -67,18 +101,12 @@ int main(int argc, char **argv)
         perror("");
     }
 
-    op_functions[HALT] = halt;
-    op_functions[JMP] = jmp;
-    op_functions[OUT] = out;
-    op_functions[NOOP] = noop;
-    op_functions[JT] = jt;
-    op_functions[JF] = jf;
-    op_functions[SET] = set;
-
     disass(fp);
 
     return 0;
 }
+
+void disass_single(uint16_t op, FILE *fp);
 
 void disass(FILE *fp)
 {
@@ -89,10 +117,7 @@ void disass(FILE *fp)
                 NUM_OP_CODES - 1, op);
             exit(1);
         }
-        if (op_functions[op] == 0) {
-            not_implemented(op);
-        }
-        op_functions[op](fp);
+        disass_single(op, fp);
     }
 }
 
@@ -153,7 +178,7 @@ void check_valid_reg(uint16_t val, const char *instruction, int arg_num)
     exit(1);
 }
 
-void check_int_or_reg(uint16_t val, const char *instruction, int arg_num)
+void check_valid_int_or_reg(uint16_t val, const char *instruction, int arg_num)
 {
     if (is_reg(val) || is_valid_int(val))
         return;
@@ -183,77 +208,62 @@ void print_value(uint16_t val)
     }
 }
 
-
-/* instruction functions */
-
-void not_implemented(enum opcode code)
+void disass_single(uint16_t op_num, FILE *fp)
 {
-    printf("*** DEBUG: Opcode number %d (%s) not implemented.\n", code, op_to_string(code));
-    exit(0);
+    struct asm_instr instr = ops[op_num];
+    int num_args = 0;
+    uint16_t arguments[MAX_NUM_ARGS_PLUS_ONE];
+    int i;
+    enum asm_instr_arg_type type;
+    
+    if (instr.special_handler) {
+        instr.special_handler(fp);
+        return;
+    }
+
+    for (i = 0; i < sizeof arguments / sizeof *arguments; ++i) {
+        type = instr.arg_types[i];
+        if (type == EMPTY)
+            break;
+        
+        arguments[i] = readU16_check(fp);
+        
+        switch (type) {
+        case REG:
+            check_valid_reg(arguments[i], instr.mnemonic, i);
+            break;
+
+        case INT:
+            check_valid_int(arguments[i], instr.mnemonic, i);
+            break;
+
+        case REG_OR_INT:
+            check_valid_int_or_reg(arguments[i], instr.mnemonic, i); 
+            break;
+
+        default:
+            fprintf(stderr, "HORRIBLE INTERNAL ERROR!");
+            exit(1);
+        }
+    }
+
+    num_args = i;
+
+    printl(num_args + 1, instr.mnemonic);
+    if (num_args) {
+        putchar('\t');
+        print_value(arguments[0]);
+    }
+    for (i = 1; i < num_args; ++i) {
+        printf(",\t");
+        print_value(arguments[i]);
+    }
+    putchar('\n');
 }
 
-void halt(FILE *fp)
-{
-    printl(1, "hlt\n");
-}
-
-void out(FILE *fp)
+void out_handler(FILE *fp)
 {
     uint16_t ch = readU16_check(fp);
-    printl(2, "out\t'%s'\n", convert_escape(ch));
+    printl(2, "out");
+    printf("\t'%s'\n", convert_escape(ch));
 }
-
-void noop(FILE *fp)
-{
-    printl(1, "noop\n");
-}
-
-void jmp(FILE *fp)
-{
-    uint16_t addr = readU16_check(fp);
-    check_int_or_reg(addr, __func__, 1);
-    printl(2, "jmp\t");
-    print_value(addr);
-    putchar('\n');
-}
-
-void jump_tf_common(FILE *fp, const char *op_name)
-{
-    uint16_t addr, cond;
-    cond = readU16_check(fp);
-    check_int_or_reg(cond, op_name, 1);
-    addr = readU16_check(fp);
-    check_int_or_reg(addr, op_name, 2);
-    printl(3, op_name);
-    putchar('\t');
-    print_value(cond);
-    putchar('\t');
-    print_value(addr);
-    putchar('\n');
-}
-
-void jt(FILE *fp)
-{
-    jump_tf_common(fp, "jt");
-}
-
-void jf(FILE *fp)
-{
-    jump_tf_common(fp, "jf");
-}
-
-void set(FILE *fp)
-{
-    uint16_t reg, val;
-    reg = readU16_check(fp);
-    check_valid_reg(reg, __func__, 1);
-    val = readU16_check(fp);
-    check_int_or_reg(val, __func__, 2);
-    printl(3, "set\t");
-    print_value(reg);
-    putchar('\t');
-    print_value(val);
-    putchar('\n');
-}
-
-
