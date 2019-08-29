7e5bf6751fa47781ca74e6107adc0fcef6625c5d
Pete Dietl <petedietl@gmail.com>
2019-08-28 23:24:37 -0500

    Update formatting to be like objump, allow for compressing runs of out commands into a single string

--- main.c
+++ main.c
@@ -5,8 +5,6 @@
 #include <ctype.h>
 #include "arch.h"
 
-void out_handler(FILE *fp);
-
 enum asm_instr_arg_type {
     EMPTY,
     REG,
@@ -14,37 +12,38 @@ enum asm_instr_arg_type {
     REG_OR_INT
 };
 
-#define MAX_NUM_ARGS_PLUS_ONE 4
+#define MAX_NUM_ARGS 3
+#define MAX_NUM_ARGS_PLUS_ONE (MAX_NUM_ARGS + 1)
 
 struct asm_instr {
     const char *mnemonic;
+    unsigned char op_code;
     enum asm_instr_arg_type arg_types[MAX_NUM_ARGS_PLUS_ONE];
-    void (*special_handler)(FILE *fp);
 };
 
 struct asm_instr ops[] = {
-    {"hlt",     {0}},
-    {"set",     {REG, REG_OR_INT}},
-    {"push",    {REG_OR_INT}},
-    {"pop",     {REG_OR_INT}},
-    {"eq",      {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
-    {"gt",      {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
-    {"jmp",     {REG_OR_INT}},
-    {"jt",      {REG_OR_INT, REG_OR_INT}},
-    {"jf",      {REG_OR_INT, REG_OR_INT}},
-    {"add",     {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
-    {"mult",    {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
-    {"mod",     {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
-    {"and",     {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
-    {"or",      {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
-    {"not",     {REG_OR_INT, REG_OR_INT}},
-    {"rmem",    {REG_OR_INT, REG_OR_INT}},
-    {"wmem",    {REG_OR_INT, REG_OR_INT}},
-    {"call",    {REG_OR_INT}},
-    {"ret",     {0}},
-    {"out",     {REG_OR_INT}, out_handler},
-    {"in",      {REG_OR_INT}},
-    {"noop",    {0}}
+    {"hlt",     0,  {0}},
+    {"set",     1,  {REG, REG_OR_INT}},
+    {"push",    2,  {REG_OR_INT}},
+    {"pop",     3,  {REG_OR_INT}},
+    {"eq",      4,  {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"gt",      5,  {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"jmp",     6,  {REG_OR_INT}},
+    {"jt",      7,  {REG_OR_INT, REG_OR_INT}},
+    {"jf",      8,  {REG_OR_INT, REG_OR_INT}},
+    {"add",     9,  {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"mult",    10, {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"mod",     11, {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"and",     12, {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"or",      13, {REG_OR_INT, REG_OR_INT, REG_OR_INT}},
+    {"not",     14, {REG_OR_INT, REG_OR_INT}},
+    {"rmem",    15, {REG_OR_INT, REG_OR_INT}},
+    {"wmem",    16, {REG_OR_INT, REG_OR_INT}},
+    {"call",    17, {REG_OR_INT}},
+    {"ret",     18, {0}},
+    {"out",     19, {REG_OR_INT}},
+    {"in",      20, {REG_OR_INT}},
+    {"noop",    21, {0}}
 };
 
 void disass(FILE *fp);
@@ -55,20 +54,28 @@ void not_implemented(enum opcode);
 int line = 0;
 bool no_offset = false;
 bool hex_digits = false;
+bool raw_instr = true;
+bool compress_outs = false;
+
+static bool last_instr_was_out = false;
 
 void usage(const char *prog_name)
 {
     printf( "%s <options...> [binary file]\n"
             "Options:\n"
-            "\t-h | --help:       Show this help message\n"
-            "\t-n | --no-offset:  Do not prefix instructions with their offset relative to the beginning of the file\n"
-            "\t-x | --hex-digits: Show the offset and all integer arguments in hex instead of decimal\n", prog_name);
+            "\t-h | --help:               Show this help message\n"
+            "\t-n | --no-offset:          Do not prefix instructions with their offset relative to the beginning of the file\n"
+            "\t-x | --hex-digits:         Show the offset and all integer arguments in hex instead of decimal\n"
+            "\t-c | --compress-out-instr: Compress runs of out instructions into a single line\n"
+            "\t-r | --no-show-raw-instr:  Do not display hex alongside symbolic disassembly\n", prog_name);
 }
 
 struct option long_options[] = {
-    {"no-offset",       optional_argument, 0, 'n'},
-    {"hex-digits",      optional_argument, 0, 'x'},
-    {"help",            optional_argument, 0, 'h'},
+    {"help",              optional_argument, 0, 'h'},
+    {"no-offset",         optional_argument, 0, 'n'},
+    {"hex-digits",        optional_argument, 0, 'x'},
+    {"compress-outs",     optional_argument, 0, 'c'},
+    {"no-show-raw-instr", optional_argument, 0, 'r'},
     {0, 0, 0, 0}
 };
 
@@ -78,7 +85,7 @@ int main(int argc, char **argv)
 
     while (1) {
         option_index = 0;
-        ch = getopt_long(argc, argv, "n::h::x::", long_options, &option_index);
+        ch = getopt_long(argc, argv, "n::h::x::c::r::", long_options, &option_index);
 
         if (ch == -1)
             break;
@@ -87,6 +94,8 @@ int main(int argc, char **argv)
             case 'h': usage(argv[0]); return 0;
             case 'n': no_offset = true; break;
             case 'x': hex_digits = true; break;
+            case 'c': compress_outs = true; break;
+            case 'r': raw_instr = false; break;
             case '?': // fallthrough
             default: exit(1);
         }
@@ -159,9 +168,9 @@ void printl(int increment, const char *format, ...)
 {
     if (!no_offset) {
         if (hex_digits)
-            printf("%#7x\t", line);
+            printf("    %04x:\t", line);
         else
-            printf("%5d\t", line);
+            printf("%8d:\t", line);
     }
     va_list args;
     va_start(args, format);
@@ -201,6 +210,11 @@ void check_valid_int_or_reg(uint16_t val, const char *instruction, int arg_num)
         arg_num, instruction, val);
 }
 
+void print_int(uint16_t n)
+{
+    printf(" %04x", n);
+}
+
 uint16_t readU16_check(FILE *fp)
 {
     uint16_t n;
@@ -217,9 +231,21 @@ void print_value(uint16_t val)
         printf("r%d", val - MIN_REG);
     } else {
         if (hex_digits)
-            printf("%#x", val);
+            printf("0x%04x", val);
         else
-            printf("%d", val);
+            printf("%6d", val);
+    }
+}
+
+void print_value_with_comma(uint16_t val)
+{
+    if (is_reg(val)) {
+        printf("r%d,     ", val - MIN_REG);
+    } else {
+        if (hex_digits)
+            printf("0x%04x, ", val);
+        else
+            printf("%6d, ", val);
     }
 }
 
@@ -230,11 +256,34 @@ void disass_single(uint16_t op_num, FILE *fp)
     uint16_t arguments[MAX_NUM_ARGS_PLUS_ONE];
     int i;
     enum asm_instr_arg_type type;
-    
-    if (instr.special_handler) {
-        instr.special_handler(fp);
+  
+    /* special case for OUT instruction */
+    if (op_num == OUT) {
+        uint16_t ch = readU16_check(fp);
+        if ((compress_outs && !last_instr_was_out) || !compress_outs) {
+            if (raw_instr) {
+                printl(num_args + 1, "%04x", instr.op_code);
+                print_int(ch);
+                printf("          \t\t%-4s\t", instr.mnemonic);
+            } else {
+                printl(num_args + 1, "%-4s\t", instr.mnemonic);
+            }
+        }
+        if (compress_outs && !last_instr_was_out)
+            printf("\"%s", convert_escape(ch));
+        else if (compress_outs)
+            printf("%s", convert_escape(ch));
+        else
+            printf("\"%s\"\n", convert_escape(ch));
+        last_instr_was_out = true;
         return;
+    } else if (compress_outs && last_instr_was_out) {
+        printf("\"\n");
+        last_instr_was_out = false;
     }
+
+    if (raw_instr)
+        printl(num_args + 1, "%04x", instr.op_code);
 
     for (i = 0; i < sizeof arguments / sizeof *arguments; ++i) {
         type = instr.arg_types[i];
@@ -260,25 +309,25 @@ void disass_single(uint16_t op_num, FILE *fp)
             fprintf(stderr, "HORRIBLE INTERNAL ERROR!");
             exit(1);
         }
+        if (raw_instr)
+            print_int(arguments[i]);
     }
 
     num_args = i;
 
-    printl(num_args + 1, instr.mnemonic);
-    if (num_args) {
-        putchar('\t');
-        print_value(arguments[0]);
-    }
-    for (i = 1; i < num_args; ++i) {
-        printf(",\t");
-        print_value(arguments[i]);
-    }
-    putchar('\n');
-}
+    for (i = 0; raw_instr && i < MAX_NUM_ARGS - num_args; ++i)
+        printf("    ");
 
-void out_handler(FILE *fp)
-{
-    uint16_t ch = readU16_check(fp);
-    printl(2, "out");
-    printf("\t'%s'\n", convert_escape(ch));
+    if (raw_instr)
+        printf("\t\t%-4s\t", instr.mnemonic);
+    else
+        printl(num_args + 1, "%-4s\t", instr.mnemonic);
+
+    for (i = 0; i < num_args - 1; ++i) {
+        print_value_with_comma(arguments[i]);
+    }
+    
+    if (num_args)
+        print_value(arguments[num_args - 1]);
+    putchar('\n');
 }
